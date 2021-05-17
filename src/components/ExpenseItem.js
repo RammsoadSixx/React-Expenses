8b82e47be7b71e691a3d4bd00f5f23423f9f93ff
Rafael Ruiz <raruiz@pgjebc.gob.mx>
2021-05-17 15:02:56 -0700

    +Se mandan componentes a través de las props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(){
+function ExpenseItem(props){
     return(
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description"> 
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
